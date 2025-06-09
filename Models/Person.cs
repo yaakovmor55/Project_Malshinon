0e139fbb94cfe86b46889efce9fa9a20614f39cb
Yaakov <y0583275590@gmail.com>
2025-06-09 15:07:37 +0300

    I added classes to "person" and "report" and arranged folders.

---
+++ Models/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Malshinon.Models
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public string CodeName { get; set; }
+        public DateTime Date { get; set; }
+
+
+        public Person(string name, string code)
+        {
+            Name = name;
+            CodeName = code;
+            Date = DateTime.Now;
+        }
+    }
+}
---
+++ Models/Report.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Malshinon.Models
+{
+    internal class Report
+    {
+        public string ReportedId { get; set; }
+        public string TargetId {  get; set; }
+        public string ReportText {  get; set; }
+        public DateTime SubmitAt { get; set; }
+
+        public Report(string reportedId, string targetId, string reportText)
+        {
+            ReportedId = reportedId;
+            TargetId = targetId;
+            ReportText = reportText;
+            SubmitAt = DateTime.Now;
+        }
+    }
+}

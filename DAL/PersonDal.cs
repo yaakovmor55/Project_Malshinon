8e51a2906ecc8c983ebc9d458fef8c444b1ae6da
Yaakov <y0583275590@gmail.com>
2025-06-09 21:18:45 +0300

    I implemented DAL for person and report

---
+++ DAL/PersonDal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Malshinon.DAL
+{
+    internal class PersonDal
+    {
+        public static void AddPerson(string FirstName, string LastName, string SecretCode, string type)
+        {
+            int num_reports = 0;
+            int num_mentions = 0;
+            var sql = $"INSERT INTO people(FirstName, LastName, SecretCode, type, num_reports, num_mentions)" +
+                $"VALUES('{FirstName}', '{LastName}', '{SecretCode}', '{type}', '{num_reports}', '{num_mentions}')"; 
+
+            DBConnection.Execute(sql);
+        }
+    }
+}
---
+++ DAL/ReportDal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Malshinon.DAL
+{
+    internal class ReportDal
+    {
+        public static void AddReport(int ReporterId, int TargetId, string ReportText)
+        {
+            DateTime SubmittedAt = DateTime.Now;
+            var sql = $"INSERT INTO reports(ReporterId, TargetId, ReportText, SubmittedAt)" +
+              $"VALUES('{ReporterId}', '{TargetId}', '{ReportText}', '{SubmittedAt:yyyy-mm-dd HH:mm:ss}')";
+
+            DBConnection.Execute(sql);
+            var updateSql = $"UPDATE people SET num_reports = num_reports + 1 WHERE Id = {ReporterId}";
+            DBConnection.Execute(updateSql);
+        }
+    }
+}
